463afc117be574dfcbce1a718b03a7899d08e335
mintuw <jacob.rohdin@gmail.com>
2023-05-02 08:53:53 +0200

    Updated the code with a temporary struct which will be changed to CAM class later

--- src/route_decider.cpp
+++ src/route_decider.cpp
@@ -10,22 +10,37 @@ const double SHORT_ROUTE = 598.34;
 const double LONG_ROUTE = 698.26;
 const int MAX_SPEED = 20;
 
+struct LatestMessage {
+    string edge;
+    double speed;
+};
+
 vector<string> edges_short = {"main1"};
 vector<string> edges_long = {"side1", "side2", "side3"};
 
-bool continue_on_route(vector<map<string, double>> latest, double short_speed, double long_speed)
+bool continue_on_route(vector<LatestMessage> latest, double short_speed, double long_speed)
 {
-    for (auto key : latest)
+    for (auto message : latest)
     {
-        if (find(edges_short.begin(), edges_short.end(), key["edge"]) != edges_short.end() && key["speed"] < short_speed)
+        if (find(edges_short.begin(), edges_short.end(), message.edge) != edges_short.end() && message.speed < short_speed)
         {
-            short_speed = key["speed"];
+            short_speed = message.speed;
         }
-        if (find(edges_long.begin(), edges_long.end(), key["edge"]) != edges_long.end() && key["speed"] < long_speed)
+        if (find(edges_long.begin(), edges_long.end(), message.edge) != edges_long.end() && message.speed < long_speed)
         {
-            long_speed = key["speed"];
+            long_speed = message.speed;
         }
     }
 
     return short_speed * SHORT_ROUTE < long_speed * LONG_ROUTE;
+}
+
+int main()
+{
+    // Example usage of continue_on_route function
+    vector<LatestMessage> latest_messages = {{"main1", 15}, {"side1", 10}, {"side2", 20}};
+    bool continue_on_short_route = continue_on_route(latest_messages, MAX_SPEED, MAX_SPEED);
+    cout << "Continue on short route? " << (continue_on_short_route ? "Yes" : "No") << endl;
+
+    return 0;
 }
