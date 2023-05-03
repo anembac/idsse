64337cdf084705c53de22b7364862f3a43ad4e59
mintuw <jacob.rohdin@gmail.com>
2023-05-03 14:38:50 +0200

    Finished functionality to work together with CAM/Reports

--- src/network_ids.cpp
+++ src/network_ids.cpp
@@ -4,39 +4,25 @@
 #include <vector>
 #include <Report.hpp>
 
+
+//This should be updated when the testing determines reasonable bounds
 const int LOWER_BOUND = 0;
 const int UPPER_BOUND = 100;
 
-struct Message
-{
-    std::vector<int> receiver_position;
-    std::vector<int> sender_position;
-    double msg_arrival_time;
-    double timestamp;
-    std::string fingerprint;
-};
 
-std::map<std::string, std::vector<Message>> messages;
-std::vector<std::string> misbehaving;
+std::vector<Report> misbehaving;
+hash_map<size_t, std::vector<Report>> messages;
 
-double
-distance(std::tuple<int> pos1, std::tuple<int> pos2)
-{
-    /*Returns the distance between two positions*/
+double distance(std::tuple<int> pos1, std::tuple<int> pos2) {
     return sqrt(pow((std::get<0>(pos2) - std::get<0>(pos1)), 2) + 
                 pow((std::get<1>(pos2) - std::get<1>(pos1)), 2));
 }
 
-bool
-detect_misbehavior(std::vector<Report> reports)
-{
-    /*Function for determining if a message is suspicious*/
-    for (auto report : reports)
-    {
+bool detect_misbehavior(std::vector<Report> reports) {
+    for (auto report : reports) {
         double dist = distance(msg.getMetaData().positionOnReceieve, msg.getCam().pos);
         double transfer_time = msg.getMetaData().timeOnReceive - msg.getCam().generationDeltaTime;
-        if (LOWER_BOUND <= dist / transfer_time && dist / transfer_time <= UPPER_BOUND)
-        {
+        if (LOWER_BOUND <= dist / transfer_time && dist / transfer_time <= UPPER_BOUND) {
             continue;
         }
         return true;
@@ -46,40 +32,26 @@ detect_misbehavior(std::vector<Report> reports)
 
 
 //Needs updating to accomadate for reports...
-void
-misbehaving_msgs()
-{
+void misbehaving_msgs() {
     /*Function responsible for going through all messages and adding misbehaving ones to a list*/
-    for (auto& [k, v] : messages)
-    {
-        if (detect_misbehavior(v))
-        {
+    for (auto& [k, v] : messages) {
+        if (detect_misbehavior(v)) {
             misbehaving.push_back(k);
         }
     }
 }
 
-void
-collect_messages(std::vector<Message> collection)
-{
-    /*A method for collecting messages*/
-    for (auto msg : collection)
-    {
-        std::string fingerprint = msg.fingerprint;
-        if (messages.count(fingerprint))
-        {
-            messages[fingerprint].push_back(msg);
-        }
-        else
-        {
-            messages.insert({fingerprint, {msg}});
-        }
+void collect_messages(std::vector<Report> reports) {
+    for (auto report : reports){
+        messages[report.getCam().fingerprint].push_back(report);
     }
 }
 
-int
-main()
-{
+void collect_single_msg(Report report) {
+    messages[report.getCam().fingerprint].push_back(report);
+}
+
+int main() {
     /*A main method responsible for handling Intrusion Detection on network level*/
     return 0;
 }
