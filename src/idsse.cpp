d67bfffc58dcf0a71f5a59d548e1f658716b8680
anembac <anembac@gmail.com>
2023-08-25 09:08:39 +0200

    changed attack

--- src/idsse.cpp
+++ src/idsse.cpp
@@ -85,11 +85,14 @@ idsse::isAttacker(std::string id){
 
 void
 idsse::triggerEvent(){
+    auto vehicleControl = deps_.getOrThrow<VehicleControlInterface, component::MissingDependency>("VehicleControlInterface", "idsse::attackStart");
     log_.info() << "Triggering event!";
     log_.info() << "isAttacker: " << isAttacker_;
-        isAttacking_ = true;
-        log_.info() << "isAttacking true, beginning to spoof";
-        caService_->spoof();
+    isAttacking_ = true;
+    log_.info() << "isAttacking true, beginning to spoof";
+    vehicleControl->setSpeed(5);
+    //caService_->spoof();
+        
 }
 
 uint64_t 
