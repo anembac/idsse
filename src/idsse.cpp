03912f8140c52326722c47218ef1c1eb8564180c
anembac <anembac@gmail.com>
2023-08-25 08:54:18 +0200

    re-enabled attacking and rerouting

--- src/idsse.cpp
+++ src/idsse.cpp
@@ -103,7 +103,7 @@ idsse::attackStart(){
     log_.info() << "Running attack start";
     auto vehicleControl = deps_.getOrThrow<VehicleControlInterface, component::MissingDependency>("VehicleControlInterface", "idsse::attackStart");
     auto es = deps_.getOrThrow<EventScheduler, component::MissingDependency>("EventScheduler", "idsse::attackStart");
-    //triggerEvent_ = es->schedule([this] () { triggerEvent();}, std::chrono::milliseconds(triggerStart_));
+    triggerEvent_ = es->schedule([this] () { triggerEvent();}, std::chrono::milliseconds(triggerStart_));
     //vehicleControl->setSpeed(15.00);
     log_.info() << "Attack start completed";
 
@@ -115,7 +115,7 @@ idsse::normalStart(){
     auto vehicleControl = deps_.getOrThrow<VehicleControlInterface, component::MissingDependency>("VehicleControlInterface", "idsse::normalStart");
     auto es = deps_.getOrThrow<EventScheduler, component::MissingDependency>("EventScheduler", "idsse:normalStart");
     //log_.info() << "Scheduling reroute with delay: " << rerouteDelay_;
-    //rerouteEvent_ = es->schedule([this] () {rerouter();},std::chrono::milliseconds(rerouteDelay_), std::chrono::milliseconds(reroutePeriod_));
+    rerouteEvent_ = es->schedule([this] () {rerouter();},std::chrono::milliseconds(rerouteDelay_), std::chrono::milliseconds(reroutePeriod_));
     log_.info() << "Scheduling speedAdapter with delay: " << speedAdapterStart_ << " and period: " << speedAdapterPeriod_;
     speedAdapterEvent_ = es->schedule([this] () {speedAdapter();},std::chrono::milliseconds(speedAdapterStart_), std::chrono::milliseconds(speedAdapterPeriod_));
     log_.info() << "Normal start completed";
