4080c81739af7b166172aefe42a6fd72722db168
anembac <anembac@gmail.com>
2023-08-26 15:57:48 +0200

    testing different values for origin

--- ezC2X/Origin.hpp
+++ ezC2X/Origin.hpp
@@ -1,7 +1,7 @@
 #ifndef IDSSE_ORIGIN
 #define IDSSE_ORIGIN
 
-const double ORIGIN_LAT = 48.13253;
-const double ORIGIN_LONG = 11.52818;
+const double ORIGIN_LAT = 48.13254;
+const double ORIGIN_LONG = 11.52821;
 
 #endif
