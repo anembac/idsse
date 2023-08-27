6e1ae9d6b884f80c9f1e93b7692664768be74dd5
anembac <anembac@gmail.com>
2023-08-27 12:43:00 +0200

    new attempt at finding correct origin

--- ezC2X/Origin.hpp
+++ ezC2X/Origin.hpp
@@ -1,7 +1,7 @@
 #ifndef IDSSE_ORIGIN
 #define IDSSE_ORIGIN
-
-const double ORIGIN_LAT = 48.13254;
-const double ORIGIN_LONG = 11.52821;
+//Coordinates as found in ns-allinone-3.35/ns-3.35/contrib/ezc2x/model/ezc2x-coordinate-projection.h
+const double ORIGIN_LAT = 48.132669;
+const double ORIGIN_LONG = 11.528325;
 
 #endif
